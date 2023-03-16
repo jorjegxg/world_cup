6ae5b27016b0d4dc7d48bd6e8a64ff099b30263f
George Luta <yotrevorgxg@gmail.com>
2023-03-16 09:39:16 +0200

    feat : add ChampionsLeague class

---
+++ WC2022/ChampionsLeague.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WC2022
+{
+    internal class ChampionsLeague : GroupTypeCompetition
+    {
+        string[] participantTeams;
+        public ChampionsLeague(string name, int numberOfTeams, int numberOfGroups, string[] _participantTeams) : base(name, numberOfTeams, numberOfGroups)
+        {
+            participantTeams = _participantTeams;
+        }
+    }
+}
