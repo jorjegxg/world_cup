da21ecd236ce31eccfa792d8d0f3a8d2c607e917
George Luta <yotrevorgxg@gmail.com>
2023-03-16 09:33:55 +0200

    feat : add GroupTypeCompetition~

---
+++ WC2022/GroupTypeCompetition.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WC2022
+{
+    class GroupTypeCompetition : Competition
+    {
+        int numberOfGroups;        
+        public GroupTypeCompetition(string _name, int _numberOfTeams, int numberOfGroups) : base(_name, _numberOfTeams)
+        {
+            this.numberOfGroups = numberOfGroups;
+        }
+    }
+    
+}
