8f59c6440c640dfb92bee1fe7bd8fb7839d7b081
George Luta <yotrevorgxg@gmail.com>
2023-03-16 09:29:47 +0200

    feat : competition class

---
+++ WC2022/Competition.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WC2022
+{
+    class Competition
+    {
+        public string name;
+        public int numberOfTeams;
+
+        public Competition(string _name , int _numberOfTeams)
+        {
+            name = _name;
+            numberOfTeams = _numberOfTeams;
+        }
+    }
+}
---
+++ WC2022/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WC2022
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var comp = new Competition("WC" , 20);
+
+
+            Console.WriteLine(comp.name + " " + comp.numberOfTeams);
+
+        }
+    }
+}
